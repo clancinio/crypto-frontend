a47947a2d6a8d8e998a52da3f85995a9c6f47c15
Clancinio <dean.clancy91@gmail.com>
2021-11-10 14:17:24 +0000

    Add route for top coins page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeContainer from "./containers/HomeContainer";
+import TopCoinsContainer from "./containers/TopCoinsContainer";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<HomeContainer />} />
+        <Route exact path="/top" element={<TopCoinsContainer />} />
       </Routes>
     </Router>
   );
