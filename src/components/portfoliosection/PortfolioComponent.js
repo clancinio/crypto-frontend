771a25e432ac3844c7595d7862143c7be0bff9c4
Clancinio <dean.clancy91@gmail.com>
2021-11-13 14:41:57 +0000

    Add Portfoliocomponent

---
+++ src/components/portfoliosection/PortfolioComponent.js
@@ -0,0 +1,12 @@
+import Col from "react-bootstrap/Col";
+
+function PortfolioComponent() {
+  return (
+    <Col className="section mt-5">
+      <h1>Portfolio</h1>
+      <p className="lead">3 Assests</p>
+    </Col>
+  );
+}
+
+export default PortfolioComponent;
