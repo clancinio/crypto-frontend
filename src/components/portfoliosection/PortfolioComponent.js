4e879c527f178162efc0b42bd41103ca47769ae3
Clancinio <dean.clancy91@gmail.com>
2021-11-13 14:58:06 +0000

    Add table for displaying assets

--- src/components/portfoliosection/PortfolioComponent.js
+++ src/components/portfoliosection/PortfolioComponent.js
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
+import "./portfolio.css";
 
 function PortfolioComponent() {
+  // State to mock assets
+  const [state, setstate] = useState([]);
+
   return (
     <Col className="section mt-5">
       <h1>Portfolio</h1>
       <p className="lead">3 Assests</p>
+      <div className="table-container">
+        <Table striped bordered>
+          <thead>
+            <tr>
+              <th>Asset</th>
+              <th>Amount</th>
+              <th>Value</th>
+              <th>Value Change (24h)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {coins.map((coin, index) => {
+              return (
+                <Coin
+                  key={coin.id}
+                  position={index}
+                  name={coin.name}
+                  price={coin.current_price}
+                  symbol={coin.symbol}
+                  marketcap={coin.total_volume}
+                  volume={coin.market_cap}
+                  image={coin.image}
+                  priceChange={coin.price_change_percentage_24h}
+                />
+              );
+            })} */}
+          </tbody>
+        </Table>
+      </div>
     </Col>
   );
 }
