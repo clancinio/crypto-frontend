02b84582389a67719123689dd18c06e8226b6f68
Clancinio <dean.clancy91@gmail.com>
2021-11-17 20:18:27 +0000

    Add Transaction component to HomeContainer

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -3,6 +3,7 @@ import TopNav from "../components/navbar/TopNav";
 import TopSection from "../components/topsection/TopSection";
 import PortfolioComponent from "../components/portfoliosection/PortfolioComponent";
 import Footer from "../components/footer/Footer";
+import TransactionComponent from "../components/transactionsection/TransactionComponent";
 
 function HomeContainer() {
   return (
@@ -11,6 +12,7 @@ function HomeContainer() {
       <Container>
         <TopSection />
         <PortfolioComponent />
+        <TransactionComponent />
       </Container>
       <Footer />
     </>
