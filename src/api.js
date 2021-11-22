508d8825f6f86e26581a8f278a385ba027d58161
Clancinio <dean.clancy91@gmail.com>
2021-11-22 19:20:33 +0000

    Change current price uri to assetdata

--- src/api.js
+++ src/api.js
@@ -5,5 +5,5 @@ const baseURL = "https://api.coingecko.com/api/v3";
 export const top100 = `${baseURL}/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
 // Get current data for a coin
-export const currrentPrice = (id) =>
+export const assetData = (id) =>
   `${baseURL}/coins/${id}?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
