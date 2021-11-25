ff8d662b5d9c6b9ca8b115d4fc3f22d28269d919
Clancinio <dean.clancy91@gmail.com>
2021-11-25 13:14:01 +0000

    Create a simpe test

---
+++ src/components/topsection/moneytotal/TotalMoneyComnponent.test.js
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import TotalMoneyComponent from "./TotalMoneyComponent";
+
+// Testin the correct content displays in component
+describe("<TotalMoneyComponent />", () => {
+  test("Renders <TotalMoneyComponent /> component correctly", () => {
+    const { getByText } = render(<TotalMoneyComponent />);
+    expect(getByText(/Total Capital/i)).toBeInTheDocument();
+  });
+});
