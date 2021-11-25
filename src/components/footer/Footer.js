de380d368a5e7ecd5fd951f4a3b720731955d1bd
Clancinio <dean.clancy91@gmail.com>
2021-11-25 13:13:29 +0000

    Cleanup

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,18 +3,18 @@ import "./footer.css";
 
 function Footer() {
   return (
-    <div class="footer-basic mt-5">
+    <div className="footer-basic mt-5">
       <footer>
         <div className="container">
-          <ul class="list-inline">
-            <li class="list-inline-item">
+          <ul className="list-inline">
+            <li className="list-inline-item">
               <Link to="/">Home</Link>
             </li>
-            <li class="list-inline-item">
+            <li className="list-inline-item">
               <Link to="/top">Top 100</Link>
             </li>
           </ul>
-          <p class="copyright">Company Name © 2018</p>
+          <p className="copyright">Company Name © 2018</p>
         </div>
       </footer>
     </div>
