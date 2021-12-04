d08a9315407089f6197dd7bb71f2d203a622d983
Clancinio <dean.clancy91@gmail.com>
2021-12-04 14:02:05 +0000

    call /asstets endpoint with axios

--- src/components/portfoliosection/PortfolioComponent.js
+++ src/components/portfoliosection/PortfolioComponent.js
@@ -9,91 +9,51 @@ import { assetData } from "../../api";
 function PortfolioComponent() {
   const [assets, setAssets] = useState([]);
 
-  async function getAssets() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => {
-        // Do as you wish with response here
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
-  }
-
-  // State to mock assets
-  const [dummyAssets, setDummyAssets] = useState([
-    {
-      asset_id: "bitcoin",
-      symbol: "BTC",
-      amount: "16",
-      value_change_24h: "+$10",
-    },
-    {
-      asset_id: "ethereum",
-      symbol: "ETH",
-      amount: "15002",
-      value_change_24h: "-$10",
-    },
-    {
-      asset_id: "ripple",
-      symbol: "XRP",
-      amount: "15002",
-      value_change_24h: "+$157",
-    },
-  ]);
-
   useEffect(() => {
-    getAssets();
+    async function getAssets() {
+      const response = await axios.get("http://localhost:4200/assets");
+      const requestedAssets = response.data.rows;
+      console.log(requestedAssets);
 
-    Promise.all(
-      dummyAssets.map(async function (asset) {
-        try {
-          const response = await axios.get(assetData(asset.asset_id));
-          let cp = response.data.market_data.current_price.eur;
-          let value = Number(cp) * Number(asset.amount);
-          return { ...asset, value: value, price: cp };
-        } catch (error) {
-          console.log(error.response.data.error);
-          throw error;
-        }
-      })
-    )
-      .then((newAssetArray) => {
-        setAssets(newAssetArray);
-      })
-      .catch((error) => {
-        console.log(error.response.data.error);
-      });
-  }, [dummyAssets]);
+      Promise.all(
+        requestedAssets.map(async function (asset) {
+          try {
+            const response = await axios.get(assetData(asset.AssetId));
+            let cp = response.data.market_data.current_price.eur;
+            let value = Number(cp) * Number(asset.Amount);
+            return { ...asset, value: value, price: cp };
+          } catch (error) {
+            console.log(error.response.data.error);
+            throw error;
+          }
+        })
+      )
+        .then((newAssetArray) => {
+          setAssets(newAssetArray);
+          console.log(newAssetArray);
+          console.log(assets);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    getAssets();
+  }, []);
 
   return (
     <Col className="section mt-5">
       <h1>Portfolio</h1>
-      <p className="lead">3 Assests</p>
+      <p className="lead">{assets.length} Assests</p>
       <div className="table-container">
         <Table striped bordered>
           <thead>
             <tr>
               <th>Asset</th>
+              <th>Symbol</th>
               <th>Amount</th>
               <th>Value</th>
-              <th>Value Change (24h)</th>
+              {/* <th>Value Change (24h)</th> */}
               <th>Price</th>
             </tr>
           </thead>
@@ -102,8 +62,9 @@ function PortfolioComponent() {
               return (
                 <Asset
                   key={index}
-                  symbol={asset.symbol}
-                  amount={asset.amount}
+                  name={asset.AssetName}
+                  symbol={asset.AssetSymbol}
+                  amount={asset.Amount}
                   value={asset.value}
                   value_change_24h={asset.value_change_24h}
                   price={asset.price}
--- src/components/portfoliosection/asset/Asset.js
+++ src/components/portfoliosection/asset/Asset.js
@@ -1,16 +1,13 @@
-function Asset({ symbol, amount, value, value_change_24h, price }) {
-  // Number formatter for euros
-  var formatter = new Intl.NumberFormat("en-IE", {
-    style: "currency",
-    currency: "EUR",
-  });
+import { formatter } from "../../../helpers";
 
+function Asset({ name, symbol, amount, value, value_change_24h, price }) {
   return (
     <tr>
+      <td>{name}</td>
       <td>{symbol.toUpperCase()}</td>
       <td>{amount}</td>
       <td>{formatter.format(value)}</td>
-      <td>{value_change_24h}</td>
+      {/* <td>{value_change_24h}</td> */}
       <td>{formatter.format(price)}</td>
     </tr>
   );
