e03739ac4efbeb2c48ed3a4d8c6487a03ee9166c
Clancinio <dean.clancy91@gmail.com>
2021-12-07 14:27:38 +0000

    Add login page

---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function LoginContainer({ setisLoggedIn }) {
+  // Formik
+  const onSubmit = () => {
+    setisLoggedIn(true);
+  };
+  return (
+    <div class="container">
+      <div className="form-container  mt-5">
+        <h1 className="text-center">Welcome back!</h1>
+        <p className="text-muted text-center">Login to acces your account</p>
+        <Form className="login-form mt-3">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="login-lable">Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="login-lable">Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Link to="/">
+            <div className="d-grid gap-2">
+              <Button
+                className="btn"
+                variant="success"
+                size="lg"
+                onClick={onSubmit}
+              >
+                Login
+              </Button>{" "}
+            </div>
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
+}
+export default LoginContainer;
