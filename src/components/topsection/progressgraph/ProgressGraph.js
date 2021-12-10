23e7ad00cf64b68792d4de22caa3f8d2c04fce92
Geoffery Yakub <60505454+S00198707@users.noreply.github.com>
2021-12-10 11:35:44 +0000

    Update ProgressGraph.js

--- src/components/topsection/progressgraph/ProgressGraph.js
+++ src/components/topsection/progressgraph/ProgressGraph.js
@@ -1,14 +1,36 @@
 import Col from "react-bootstrap/Col";
 import "./progressgraph.css";
+import { Chart } from "react-google-charts";
+
 
 function ProgressGraph() {
   return (
     <Col md={6}>
       <div className="section">
-        <h1>Graph goes here</h1>
+      <Chart
+    width={500}
+    height={'300px'}
+    chartType="AreaChart"
+    loader={<div>Loading Chart</div>}
+    data={[
+      ['Year', 'Balance'], 
+      ['1H',  53000],
+      ['1D',  35721],
+      ['1W',  105000],
+      ['1Y',  25000],
+      ['All',  155000],
+    ]}
+    options={{
+      title: 'Portfolio Balance',
+      hAxis: { title: '', titleTextStyle: { color: '#333' } },
+      vAxis: { minValue: 0 },
+      // For the legend to fit, we make the chart area smaller
+      chartArea: { width: '78%', height: '70%' },
+      
+    }}
+  />
       </div>
     </Col>
   );
 }
-
 export default ProgressGraph;
