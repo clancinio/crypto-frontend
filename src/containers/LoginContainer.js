3bf09de5a96b97d5ec50a4a78295dc4a8bd800aa
Clancinio <dean.clancy91@gmail.com>
2022-01-19 16:28:32 +0000

    Add login logic

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import UserPool from "../UserPool";
 
 function LoginContainer({ setisLoggedIn }) {
-  // Formik
-  const onSubmit = () => {
-    setisLoggedIn(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const user = new CognitoUser({
+      Username: email,
+      Pool: UserPool,
+    });
+
+    const authDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
+    });
+
+    user.authenticateUser(authDetails, {
+      onSuccess: (data) => {
+        console.log("onSuccess: ", data);
+        setisLoggedIn(true);
+      },
+      onFailure: (err) => {
+        console.error("onFailure: ", err);
+      },
+      newPasswordRequired: (data) => {
+        console.log("newPasswordRequired: ", data);
+      },
+    });
   };
+
   return (
     <div class="container">
       <div className="form-container  mt-5">
@@ -17,12 +45,22 @@ function LoginContainer({ setisLoggedIn }) {
           <hr />
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label className="login-lable">Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label className="login-lable">Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </Form.Group>
 
           <Link to="/">
