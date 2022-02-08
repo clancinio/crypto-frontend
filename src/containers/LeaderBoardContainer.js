f359561ce87ad9d83fafea7273f95564e344aa07
Dale <S00123318@mail.itsligo.ie>
2022-02-08 11:54:03 +0000

    leaderBoard page mapped with dummy data

--- src/containers/LeaderBoardContainer.js
+++ src/containers/LeaderBoardContainer.js
@@ -5,9 +5,37 @@ import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import axios from "axios";
 import Footer from "../components/footer/Footer";
+import User from "../components/user/User";
+
 
 function LeaderBoardContainer() {
   
+  const [users, setUsers] = useState([
+    {
+      username: "bob",
+      balance: 4000
+    },
+    {
+      username: "bob",
+      balance: 4000
+    },
+    {
+      username: "bob",
+      balance: 4000
+    },
+    {
+      username: "bob",
+      balance: 4000
+    },
+    {
+      username: "bob",
+      balance: 4000
+    },
+    {
+      username: "bob",
+      balance: 4000
+    }
+  ])
   return (
 
     <>
@@ -15,10 +43,7 @@ function LeaderBoardContainer() {
     <Table className="table-dark mt-5" >
       <thead>
         <tr>
-    <th><Button href="#" variant="success">Overall</Button></th>
-    <th><Button href="#" variant="success">Weekly</Button></th>
-    <th><Button href="#" variant="success">Monthly</Button></th>
-    <th width = "1000" className="text-center text-md-right"><Button href="#"  variant="success">Your rank is ... </Button></th>
+    <th width = "1000" className=" text-md-right"><Button variant="success">Your rank is: 5</Button></th>
     </tr>
     </thead>
     </Table>
@@ -32,17 +57,19 @@ function LeaderBoardContainer() {
       <th>#</th>
       <th>UserName</th>
       <th>Balance</th>
-      <th>% Change</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    
+  {users.map((user, index) => {
+              return (
+                <User
+                  key={index}
+                  position={index}
+                  username={user.username}
+                  balance={user.balance}
+                />
+              );
+            })}
   </tbody>
 </Table>
 </Container>
