6c6c519c004c5aeb250dbef293f53e34ea8db188
Geoffery <S00198707@mail.itsligo.ie>
2022-02-17 21:10:14 +0000

    add all users and transactions section

--- src/containers/AdminContainer.js
+++ src/containers/AdminContainer.js
@@ -1,10 +1,35 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import { Col, Row } from "react-bootstrap";
+ 
 function AdminContainer() {
   return (
-    <div>
-      <h1>AdminPage</h1>
-    </div>
+    <Container>
+      <Col className={"mt-5 section"}>
+      <Row p={5}>
+        <h1>Admin</h1>
+        <Col>
+          <div class="sub-section" mt={3} md={6} sm={12}>
+            <h2>Totals transactions</h2>
+            <p className="lead">100,00</p>
+          </div>
+        </Col>
+        <Col>
+          <div class="sub-section" mt={3} md={6} sm={12}>
+            <h2>Total Users</h2>
+            <p className={`lead`}>
+              500
+            </p>
+          </div>
+        </Col>
+      </Row>
+      </Col>
+      <Col className="section mt-5">
+
+        section
+      </Col>
+
+    </Container>
   );
 }
 
