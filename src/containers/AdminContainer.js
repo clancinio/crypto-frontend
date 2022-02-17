b38c5ef8595343e105a944c71251b9bf13ce2121
Geoffery <S00198707@mail.itsligo.ie>
2022-02-17 21:26:20 +0000

    added form

--- src/containers/AdminContainer.js
+++ src/containers/AdminContainer.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { Col, Row } from "react-bootstrap";
- 
+import { Col, Row, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
 function AdminContainer() {
   return (
     <Container>
@@ -26,7 +28,51 @@ function AdminContainer() {
       </Col>
       <Col className="section mt-5">
 
-        section
+      <h1>Add New User</h1>
+      <Form className="sub-section mt-3">
+      <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label className="login-lable text-white">Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter a username"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="login-lable text-white">Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="login-lable text-white">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+            <Form.Label className="login-lable text-white">Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Re-type password"
+            />
+          </Form.Group>
+            <Link to="/">
+              <div className="d-grid gap-2">
+                <Button
+                  className="btn"
+                  variant="success"
+                  size="lg"
+                >
+                  Submit
+                </Button>{" "}
+              </div>
+            </Link>
+          </Form>
       </Col>
 
     </Container>
